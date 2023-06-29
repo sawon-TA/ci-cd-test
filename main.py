566fd1b0bdc259512e49bd8ce7deb82ba55429e4
sawon-TA <sawon.bhattachar@tigeranalytics.com>
2023-06-29 10:08:11 +0530

    main added for main branch

---
+++ main.py
@@ -0,0 +1,9 @@
+from arithmatic import add
+
+if __name__ == "__main__":
+    # Call the functions and perform arithmetic operations
+    num1 = 10
+    num2 = 5
+
+    result_add = add(num1, num2)
+    print("Addition:", result_add)
